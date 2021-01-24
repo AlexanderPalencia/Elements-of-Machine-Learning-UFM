4ee82ef607beca4f11505077991772b67bfadd68
AlexanderPalencia <mapg23@outlook.com>
2021-01-23 20:55:10 -0600

    [feat] adding problem

---
+++ Labs/Lab1/problema2.py
@@ -0,0 +1,5 @@
+cache = {}
+def recurrence_relation(n,m):
+    pass
+
+print(recurrence_relation(10,5))
---
+++ Labs/Lab1/problema4.py
@@ -0,0 +1,19 @@
+'''
+The function first generate a set of cronological numbers, the most efficient way to search the primes numbers is to generate a set of the multiples of each number in the set for later with the difference_update function only keep the numbers that are not repeated in both lists.
+
+reference:https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n
+'''
+def list_primes(n):
+    list_numbers = set(range(2, n + 1, 1))
+    list_numbers.difference_update(set(range(0, n + 1, 2)))
+    primes = [1]
+    while list_numbers:
+        eval_num = list_numbers.pop()
+        primes.append(eval_num)
+        list_numbers.difference_update(set(range(eval_num * 2, n + 1, eval_num)))
+    return primes
+
+import time
+start = time.time()
+list_primes(100000)
+print('Computed in: ', time.time() - start)
